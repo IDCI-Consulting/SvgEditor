977a2fb56837505af206c444dda69c80f1426a2c
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2016-07-22 14:42:44 +0200

    Implements getCanvasImage on SvgImageReader

--- src/class/ImageReader/SvgImageReader.js
+++ src/class/ImageReader/SvgImageReader.js
@@ -4,7 +4,25 @@
 define(['./AbstractImageReader'], function (AbstractImageReader) {
 
   return class SvgImageReader extends AbstractImageReader {
-    
+
+    /**
+     * Get the canevas image object from the svg file
+     *
+     * @param file: the svg from the input
+     * @param callback: the function with the retrieved image
+     */
+    getCanvasImage(file, callback) {
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        fabric.loadSVGFromString(event.target.result, function(objects, options) {
+          var obj = fabric.util.groupSVGElements(objects, options);
+
+          return callback(obj);
+        });
+      }
+
+      reader.readAsText(file);
+    }
   }
 
 });

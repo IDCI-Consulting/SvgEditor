90af2ccad285d6bc4ca91cf2d7bd289f6fc83141
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2016-10-11 14:17:25 +0200

    Prevent start function when the plugin is disable

--- src/plugins/ImageLoader/ImageLoaderPlugin.js
+++ src/plugins/ImageLoader/ImageLoaderPlugin.js
@@ -53,18 +53,20 @@ define(
        * Start the plugin
        */
       start() {
-        this.canvas.on('after:render', () => {
-          this.imageInput.value = ""; // reset the file input to allow to add the same file several times
-        });
-
-        this.imageInput.onchange = (e) => {
-          let file = e.target.files[0];
-          let imageReaderRegistry = new ImageReaderRegistry();
-          let imageReader = imageReaderRegistry.guessImageReader(file.type);
-          imageReader.getCanvasImage(file, (item) => {
-            this.canvas.centerObject(item);
-            this.canvas.add(item);
+        if (this.config.image_loader.enable === true) {
+          this.canvas.on('after:render', () => {
+            this.imageInput.value = ""; // reset the file input to allow to add the same file several times
           });
+
+          this.imageInput.onchange = (e) => {
+            let file = e.target.files[0];
+            let imageReaderRegistry = new ImageReaderRegistry();
+            let imageReader = imageReaderRegistry.guessImageReader(file.type);
+            imageReader.getCanvasImage(file, (item) => {
+              this.canvas.centerObject(item);
+              this.canvas.add(item);
+            });
+          }
         }
       }
     }

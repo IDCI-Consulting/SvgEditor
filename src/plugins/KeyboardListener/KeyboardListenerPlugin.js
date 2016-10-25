ca9a23025749f9ac445e247397181daaf21f986c
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2016-10-25 17:04:58 +0200

    Enhance KeyboardListenerPlugin by adding a feature to move the selected object thanks to the arrow keys

--- src/plugins/KeyboardListener/KeyboardListenerPlugin.js
+++ src/plugins/KeyboardListener/KeyboardListenerPlugin.js
@@ -28,6 +28,10 @@ define(
           if (typeof this.config.keyboard_listener.enable_delete_object !== 'boolean') {
             errors.push('keyboard_listener.enable_delete_object must be defined as a boolean');
           }
+
+          if (typeof this.config.keyboard_listener.enable_move_object !== 'boolean') {
+            errors.push('keyboard_listener.enable_move_object must be defined as a boolean');
+          }
         }
 
         return errors;
@@ -37,9 +41,10 @@ define(
        * Start the plugin
        */
       start() {
-        if (this.config.keyboard_listener.enable_delete_object === true) {
-          document.addEventListener("keydown", (event) => {
-            var keyId = event.keyCode;
+        document.addEventListener("keydown", (event) => {
+
+          if (this.config.keyboard_listener.enable_delete_object === true) {
+            let keyId = event.keyCode;
             // backspace -> 8
             // delete    -> 46
             if (keyId === 46) {
@@ -48,12 +53,44 @@ define(
                 element.remove();
               }
             }
-          });
-        }
+          }
+
+          if (this.config.keyboard_listener.enable_move_object === true) {
+            let activeObject = this.canvas.getActiveObject();
+            if (typeof activeObject !== 'undefined') {
+              let arrowKeys = [37, 38, 39, 40];
+              let keyId = event.keyCode;
+              if (arrowKeys.indexOf(keyId) !== -1) {
+                event.preventDefault();
+                let newLeft = activeObject.left;
+                let newTop = activeObject.top;
+                switch(keyId) {
+                  case 37: // left arrow
+                    newLeft = newLeft - 5;
+                    activeObject.set({left: newLeft});
+                    this.canvas.renderAll();
+                    break;
+                  case 38: // up arrow
+                    newTop = newTop - 5;
+                    activeObject.set({top: newTop});
+                    this.canvas.renderAll();
+                    break;
+                  case 39: // right arrow
+                    newLeft = newLeft + 5;
+                    activeObject.set({left: newLeft});
+                    this.canvas.renderAll();
+                    break;
+                  case 40: // down arrow
+                    newTop = newTop + 5;
+                    activeObject.set({top: newTop});
+                    this.canvas.renderAll();
+                }
+              }
+            }
+          }
+
+        });
       }
-
-      
     }
-
   }
 );

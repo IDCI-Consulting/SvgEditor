659a77c227c06b2cdeb4d52a772b038b116891ca
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2016-10-26 14:21:09 +0200

    Fix a bug on auto resize (scale was not taken in account)

--- src/plugins/AutoImageResizer/AutoImageResizerPlugin.js
+++ src/plugins/AutoImageResizer/AutoImageResizerPlugin.js
@@ -37,7 +37,29 @@ define(
        * Start the plugin
        */
       start() {
+        if (this.config.auto_image_resizer.enable === true) {
+          this.canvas.on('object:added', (event) => {
+            let object = event.target;
+            let canvasWidth = parseInt(getComputedStyle(document.getElementById(this.config.canvas_id)).width);
+            let canvasHeight = parseInt(getComputedStyle(document.getElementById(this.config.canvas_id)).height);
 
+            if (object.width * object.scaleX > canvasWidth) { // the object is too large for the canvas so we resize it automatically
+              let ratio = canvasWidth/object.width*0.90/object.scaleX;
+              object.scaleX = ratio * object.scaleX;
+              object.scaleY = ratio * object.scaleY;
+              this.canvas.centerObject(object);
+              object.setCoords();
+            }
+
+            if (object.height * object.scaleY > canvasHeight) { // the object is too large for the canvas so we resize it automatically
+              let ratio = canvasHeight/object.height*0.90/object.scaleX;
+              object.scaleX = ratio * object.scaleX;
+              object.scaleY = ratio * object.scaleY;
+              this.canvas.centerObject(object);
+              object.setCoords();
+            }
+          });
+        }
       }
     }
   }
